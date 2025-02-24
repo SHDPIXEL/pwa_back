3f9e1421bf5daa276fa0705ab48e8656ae4606dd
SHDPIXEL <shdpixel@gmail.com>
2025-02-24 22:12:49 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("reboot", "root", "", {
-  host: "localhost",
+const sequelize = new Sequelize("bereboot", "admin_bereboot", "T14wAql4UU1Kp67OzIAx", {
+  host: "bereboot-pwa.ch26co64cgxa.ap-south-1.rds.amazonaws.com",
   dialect: "mysql",
 });
 
